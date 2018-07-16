a98ff651160e12b3a44d021f2bb62a79a61d7029
Calvin Berggren <cl@calvinlesli.com>
2018-07-16 13:36:18 -0500

    Initial Code to Start on Project
    Contains definitions for data structures to be used in the project and an algorithm for creating an initial schedule candidate. Also includes sample files showing how to store input information.

---
+++ DataStructures.py
@@ -0,0 +1,130 @@
+class FacultyCourses:
+    def __init__(self, line):
+        line = line.strip()
+        elements = line.split(',')
+        self.name = elements.pop(0)
+        self.courses = elements.copy()
+    
+    def contains(self, course):
+        return course in self.courses
+    
+    def print(self):
+        print(self.name, 'has courses ', end = '')
+        for course in self.courses:
+            print(course, ' ', end = '', sep = '')
+
+
+class AllFacultyCourses:
+    def __init__(self):
+        self.allFacCourses = []
+    
+    def append(self, facCourses):
+        self.allFacCourses.append(facCourses)
+    
+    def getFacCourses(self, name):
+        if type(name) == int:
+            return self.allFacCourses[name]
+        elif type(name) == str:
+            for facCourses in self.allFacCourses:
+                if facCourses.name == name:
+                    return facCourses
+            return None
+        return None
+    
+    def getFacByCourse(self, course):
+        for facCourses in self.allFacCourses:
+            if facCourses.contains(course):
+                return facCourses
+        return None
+    
+    def getFacNameByCourse(self, course):
+        facCourses = self.getFacByCourse(course)
+        return facCourses.name
+    
+    def getFacOtherCoursesByCourse(self, course):
+        facCourses = self.getFacByCourse(course)
+        courses = []
+        for el in facCourses.courses:
+            if el != course:
+                courses.append(el)
+        return courses
+    
+    def print(self):
+        for facCourses in self.allFacCourses:
+            facCourses.print()
+            print()
+
+
+class CourseTimes:
+    def __init__(self, line):
+        line = line.strip()
+        elements = line.split(',')
+        self.name = elements.pop(0)
+        self.maxsize = elements.pop(0)
+        self.times = elements.copy()
+    
+    def contains(self, time):
+        return time in self.times
+    
+    def print(self):
+        print(self.name, 'has available times ', end = '')
+        for time in self.times:
+            print(time, ' ', end = '', sep = '')
+
+
+class AllCourseTimes:
+    def __init__(self):
+        self.allCourseTimes = []
+    
+    def append(self, courseTimes):
+        self.allCourseTimes.append(courseTimes)
+    
+    def getAllCourses(self):
+        return [courseTimes.name for courseTimes in self.allCourseTimes]
+    
+    def getCourseTimes(self, name):
+        if type(name) == int:
+            return self.allCourseTimes[name]
+        elif type(name) == str:
+            for courseTimes in self.allCourseTimes:
+                if courseTimes.name == name:
+                    return courseTimes
+            return None
+        return None
+    
+    def print(self):
+        for courseTimes in self.allCourseTimes:
+            courseTimes.print()
+            print()
+
+
+class Conflict:
+    def __init__(self, course1, course2, priority):
+        if course1 > course2:
+            course1, course2 = course2, course1
+        self.course1 = course1
+        self.course2 = course2
+        self.priority = priority
+    
+    def print(self):
+        print(self.course1, 'and', self.course2, 'conflict with priority', self.priority)
+
+
+class AllConflicts:
+    def __init__(self):
+        self.allConflicts = []
+    
+    def append(self, course1, course2, priority):
+        self.allConflicts.append(Conflict(course1, course2, priority))
+    
+    def getPenalty(self, course1, course2):
+        if course1 > course2:
+            course1, course2 = course2, course1
+        for conflict in self.allConflicts:
+            if course1 == conflict.course1 and course2 == conflict.course2:
+                return conflict.priority
+        return 0
+    
+    def print(self):
+        for conflict in self.allConflicts:
+            conflict.print()
